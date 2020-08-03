576156eb584059802d7cee4eaa43ac352f5dad2e
Noal Brown <noalbrown1@gmail.com>
2020-08-03 15:36:53 -0500

    noDBpractice

---
+++ src/components/Body.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class Body extends Component {
+  constructor() {
+    super()
+    this.state = {
+      workbench: []
+    }
+  }
+
+  render() {
+    return (
+      <div className="body">
+
+      </div>
+    )
+  }
+}
+
+export default Body
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div>
+        <h1 id="header-h1">Lightsaber Builder</h1>
+      </div>
+      <div>
+        <nav id="header-nav">MENU &#9776;</nav>
+      </div>
+    </header>
+  )
+}
+
+export default Header
